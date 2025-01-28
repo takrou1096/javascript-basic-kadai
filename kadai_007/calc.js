8992f887f8674739874209ed15f3360814fcb639
takurou Yonekura <takurou11009966@gmail.com>
2025-01-28 20:13:59 +0900

    追加

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,21 +1,3 @@
-let num =  15;
-console.log(num);
-if (num % 3 == 0 && num % 5 == 0) {
-  console.log("3と5の倍数です")
-}
-
-let num = 3;
-console.log(num);
-if (num % 3 == 0) {
-  console.log("3の倍数です")
-}
-
-let num = 5;
-console.log(num);
-if (num % 5 == 0) {
-  console.log("5の倍数です")
-}
-
 let num = 11;
 console.log(num);
 if (num % 5 == 0 && num % 3 == 0) {
