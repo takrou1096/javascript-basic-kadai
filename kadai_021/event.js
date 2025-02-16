48897d68bb6450eade06245e636f630056ae4020
takurou Yonekura <takurou11009966@gmail.com>
2025-02-16 15:50:45 +0900

    追加

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -3,6 +3,6 @@ const text = document.getElementById('text');
 
 btn.addEventListener('click', () => {
   setTimeout(() => {
-    console.log("ボタンをクリックしました");
+    text.textContent("ボタンをクリックしました");
   }, 2000);
 });
